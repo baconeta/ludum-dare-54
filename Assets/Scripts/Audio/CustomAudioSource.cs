4ece3219e75ad064e4151aa95c673b3de356081c
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2023-09-28 22:30:42 +0300

    Initial commit

---
+++ Assets/Scripts/Audio/CustomAudioSource.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class CustomAudioSource : MonoBehaviour
+    {
+        private AudioSource _self;
+
+        private void RemoveFromScene()
+        {
+            DefaultDestruction(gameObject);
+        }
+
+        private void DefaultDestruction(Object toDestroy)
+        {
+            Destroy(toDestroy);
+        }
+
+        public void Init(AudioMixerGroup group)
+        {
+            if (!_self) _self = GetComponent<AudioSource>();
+
+            _self.outputAudioMixerGroup = group;
+        }
+
+        public void PlayOnce(AudioClip clip)
+        {
+            if (!_self) _self = GetComponent<AudioSource>();
+            _self.PlayOneShot(clip);
+            StartCoroutine(ResetObject(clip.length + 0.5f));
+        }
+
+        public void PlayLooping(AudioClip clip)
+        {
+            if (!_self) _self = GetComponent<AudioSource>();
+            _self.clip = clip;
+            _self.loop = true;
+            _self.Play();
+        }
+
+        public void StopAudio()
+        {
+            StartCoroutine(ResetObject(0f));
+        }
+
+        private IEnumerator ResetObject(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+
+            // Now recycle the object
+            RemoveFromScene();
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,62 @@
+﻿using Audio;
+using UI.Settings;
+using UnityEngine;
+using UnityEngine.Audio;
+using Utils;
+
+namespace Managers
+{
+    public sealed class AudioManager : EverlastingSingleton<AudioManager>
+    {
+        [SerializeField] private GameObject audioSourceObject;
+
+        [Header("Mixers")] [SerializeField] private AudioMixer masterMixer;
+
+        public const string MusicKey = "MusicVolume";
+        public const string SfxKey = "SfxVolume";
+        public const string AmbientKey = "AmbientVolume";
+
+        /// <summary>
+        /// Note that the object returned from this call will nullify itself if not looping
+        /// </summary>
+        public CustomAudioSource Play(AudioClip clip, AudioMixerGroup mixerGroup, bool looping = true)
+        {
+            if (audioSourceObject is null)
+            {
+                Debug.LogError("No custom object for audio");
+                return null;
+            }
+
+            GameObject gO = Instantiate(audioSourceObject);
+            CustomAudioSource audioSource = gO.AddComponent<CustomAudioSource>();
+            audioSource.Init(mixerGroup);
+            if (looping)
+            {
+                audioSource.PlayLooping(clip);
+            }
+            else
+            {
+                audioSource.PlayOnce(clip);
+            }
+
+            return audioSource;
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            LoadVolumes();
+        }
+
+        private void LoadVolumes() // Volume is saved in VolumeSettings.cs
+        {
+            float musicVol = PlayerPrefs.GetFloat(MusicKey, 0.5f);
+            float sfxVol = PlayerPrefs.GetFloat(SfxKey, 0.5f);
+            float ambientVol = PlayerPrefs.GetFloat(AmbientKey, 0.5f);
+
+            masterMixer.SetFloat(VolumeSettings.MixerMusic, Mathf.Log10(musicVol) * 20);
+            masterMixer.SetFloat(VolumeSettings.SfxMusic, Mathf.Log10(sfxVol) * 20);
+            masterMixer.SetFloat(VolumeSettings.AmbientMusic, Mathf.Log10(ambientVol) * 20);
+        }
+    }
+}
