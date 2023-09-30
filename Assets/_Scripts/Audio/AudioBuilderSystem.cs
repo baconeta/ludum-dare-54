598b7bc9504d97b8c4c33c49a145ac8952e9f696
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2023-09-30 16:44:50 +0300

    Test track adding and playing in sync

---
+++ Assets/_Scripts/Audio/AudioBuilderSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Audio
+{
+    public class AudioBuilderSystem : MonoBehaviour
+    {
+        [SerializeField] private CustomAudioSource _customAudioSource;
+        private List<AudioClip> _builtClips;
+
+        private void Awake()
+        {
+            _builtClips = new List<AudioClip>();
+        }
+
+        public void AddClipToBuilder(AudioClip clip)
+        {
+            _builtClips.Add(clip);
+        }
+        
+        public void PlayBuiltClips()
+        {
+            foreach (AudioClip clip in _builtClips)
+            {
+                _customAudioSource.PlayOnce(clip);
+            }
+        }
+    }
+}
