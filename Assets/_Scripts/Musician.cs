b70a1b7a1bc893bf736592cd536e01463a4d3fb1
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2023-09-30 17:58:39 +0300

    Add some new parameters to Musician class

--- Assets/_Scripts/Musician.cs
+++ Assets/_Scripts/Musician.cs
@@ -1,59 +1,146 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using Random = UnityEngine.Random;
 
 #region Demographics
+
 [Serializable]
 public enum Gender
 {
-    Male, Female, Transgender, Nonbinary, Genderfluid, TwoSpirit, Agender, Bigender, Demigender, Androgynous, Pangender, Nonconforming, Intersex, Cisgender, Genderqueer, Questioning, Other, NumOfElements
+    Male,
+    Female,
+    Transgender,
+    Nonbinary,
+    Genderfluid,
+    TwoSpirit,
+    Agender,
+    Bigender,
+    Demigender,
+    Androgynous,
+    Pangender,
+    Nonconforming,
+    Intersex,
+    Cisgender,
+    Genderqueer,
+    Questioning,
+    Other,
+    NumOfElements
 }
 
 [Serializable]
 public enum FirstNames
 {
-    Emma, Liam, Olivia, Noah, Sophia, Jackson, Ava, Aiden, Isabella, Lucas, Mia, Ethan, Harper, Oliver, Amelia, Elijah, Charlotte, Logan, Mason, NumOfElements
+    Emma,
+    Liam,
+    Olivia,
+    Noah,
+    Sophia,
+    Jackson,
+    Ava,
+    Aiden,
+    Isabella,
+    Lucas,
+    Mia,
+    Ethan,
+    Harper,
+    Oliver,
+    Amelia,
+    Elijah,
+    Charlotte,
+    Logan,
+    Mason,
+    NumOfElements
 }
+
 [Serializable]
 public enum Nicknames
 {
-    Bubbles, Sunshine, Sparky, Tinkerbell, Chuckles, Snickers, Dizzy, Giggles, Wiggles, Jellybean, Noodle, Peaches, Bouncy, Tootsie, Pickles, Twinkle, Doodlebug, Cupcake, Pudding, Sprout, NumOfElements
+    Bubbles,
+    Sunshine,
+    Sparky,
+    Tinkerbell,
+    Chuckles,
+    Snickers,
+    Dizzy,
+    Giggles,
+    Wiggles,
+    Jellybean,
+    Noodle,
+    Peaches,
+    Bouncy,
+    Tootsie,
+    Pickles,
+    Twinkle,
+    Doodlebug,
+    Cupcake,
+    Pudding,
+    Sprout,
+    NumOfElements
 }
+
 [Serializable]
 public enum LastNames
 {
-    Smith, Johnson, Williams, Jones, Brown, Davis, Miller, Wilson, Moore, Taylor, Anderson, Thomas, Jackson, White, Harris, Martin, Thompson, Garcia, Martinez, Robinson, NumOfElements
+    Smith,
+    Johnson,
+    Williams,
+    Jones,
+    Brown,
+    Davis,
+    Miller,
+    Wilson,
+    Moore,
+    Taylor,
+    Anderson,
+    Thomas,
+    Jackson,
+    White,
+    Harris,
+    Martin,
+    Thompson,
+    Garcia,
+    Martinez,
+    Robinson,
+    NumOfElements
 }
+
+[Serializable]
+public enum FacingDirection
+{
+    Left,
+    Right,
+    Forward
+}
+
 #endregion
 
 public class Musician : MonoBehaviour
 {
     private MusicianPointer worldObject;
-    [Header("Musician Bio")]
-    public string musicianNameFirst;
+    [Header("Musician Bio")] public string musicianNameFirst;
     public string musicianNameNickname;
     public string musicianNameLast;
     public int age;
     public Gender gender;
     public string bio;
+    public FacingDirection worldFacingDirection = FacingDirection.Forward;
+
+    [SerializeField] public List<Instrument.InstrumentType> proficientInstruments;
+    [SerializeField] public List<Instrument.InstrumentType> badInstruments;
 
     private void Awake()
     {
         worldObject = GetComponentInChildren<MusicianPointer>();
         worldObject.parentMusician = this;
-
     }
 
     // Start is called before the first frame update
     public Musician GenerateMusician()
     {
-        musicianNameFirst = ((FirstNames)Random.Range(0, (int)FirstNames.NumOfElements)).ToString();
-        musicianNameNickname = ((Nicknames)Random.Range(0, (int)Nicknames.NumOfElements)).ToString();
-        musicianNameLast = ((LastNames)Random.Range(0, (int)LastNames.NumOfElements)).ToString();
+        musicianNameFirst = ((FirstNames) Random.Range(0, (int) FirstNames.NumOfElements)).ToString();
+        musicianNameNickname = ((Nicknames) Random.Range(0, (int) Nicknames.NumOfElements)).ToString();
+        musicianNameLast = ((LastNames) Random.Range(0, (int) LastNames.NumOfElements)).ToString();
 
         return this;
     }
@@ -62,6 +149,4 @@ public class Musician : MonoBehaviour
     {
         return "I like horses.";
     }
-
-
 }
