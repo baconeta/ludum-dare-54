a2e6a073231e660ff637e7f1c0f8232c1bb8392d
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2023-09-30 18:14:35 +0300

    Update Musician.cs

--- Assets/_Scripts/Musician.cs
+++ Assets/_Scripts/Musician.cs
@@ -118,13 +118,15 @@ public enum FacingDirection
 public class Musician : MonoBehaviour
 {
     private MusicianPointer worldObject;
-    [Header("Musician Bio")] public string musicianNameFirst;
+    
+    [Header("Musician Bio")]
+    public string musicianNameFirst;
     public string musicianNameNickname;
     public string musicianNameLast;
     public int age;
     public Gender gender;
     public string bio;
-    public FacingDirection worldFacingDirection = FacingDirection.Forward;
+    public FacingDirection worldFacingDirection = FacingDirection.Forward; // Will determine if we should flip a char based on stage position
 
     [SerializeField] public List<Instrument.InstrumentType> proficientInstruments;
     [SerializeField] public List<Instrument.InstrumentType> badInstruments;
