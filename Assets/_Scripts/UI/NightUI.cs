841a75f304ca71258fe98c98c708b36b86e2a136
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2023-10-02 00:38:44 +0300

    game scene, night UI and some canvas scaling changes that have overcomplicated many things yay me

--- Assets/_Scripts/UI/NightUI.cs
+++ Assets/_Scripts/UI/NightUI.cs
@@ -10,4 +10,13 @@ public class NightUI : MonoBehaviour
     public TextMeshProUGUI questText;
     public TextMeshProUGUI composerNameText;
     public Button button;
+    public Image[] stars;
+    public Sprite emptyStar;
+    public Sprite halfStar;
+    public Sprite fullStar;
+
+    public void FillStars(ReviewManager.StarRating value)
+    {
+        // TODO fill the stars based on the previous scores / recent score
+    }
 }
