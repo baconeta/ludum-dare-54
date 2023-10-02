656aa088e35efc8ddddab0c6a8d2513ce1bea0a9
Brayden Cooke <sabrinalcooke@gmail.com>
2023-10-02 14:59:11 +1300

    Musician Select UI now hides on a performance starting

--- Assets/_Scripts/Gameplay/PerformanceLibrary.cs
+++ Assets/_Scripts/Gameplay/PerformanceLibrary.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 
 public class PerformanceLibrary : MonoBehaviour
 {
+    [Header("A Library Class for ALL performances.")]
+    [Header("Used to search for performanceKeys in saving/loading")]
     public PerformanceDataSO[] performances;
     public static PerformanceDataSO[] performancesStatic;
 
-    private void Start()
+    private void Awake()
     {
         performancesStatic = performances;
     }
