48f0b33e0511a3453bbc0db28a78fd1fb8e1802e
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2023-10-03 11:42:04 +0300

    fix some audio and prefab issues between scenes

--- Assets/_Scripts/TitleScreenPlay.cs
+++ Assets/_Scripts/TitleScreenPlay.cs
@@ -1,15 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
+using Managers;
 using UnityEngine;
 
-public class TItleScreenPlay : MonoBehaviour
+public class TitleScreenPlay : MonoBehaviour
 {
     public SceneLoader sceneLoader;
     public void Play()
     {
+        Destroy(FindObjectOfType<AudioManager>().gameObject);
+        
         //Go straight to orchestra if finished tutorial, else start tutorial.
         sceneLoader.LoadScene(PlayerPrefs.GetInt("TutorialComplete") > 0 
             ? SceneLoader.Scene.Orchestra : SceneLoader.Scene.Tutorial);
     }
-
 }
