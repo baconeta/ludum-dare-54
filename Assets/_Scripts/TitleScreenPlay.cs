51e58063aa8eb55273b45dc06862eb2d17efcb72
Josh Pearson <joshuapearsonnz@gmail.com>
2023-10-06 20:56:12 +0300

    Fixed play button positioning

--- Assets/_Scripts/TitleScreenPlay.cs
+++ Assets/_Scripts/TitleScreenPlay.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 public class TitleScreenPlay : MonoBehaviour
 {
     public SceneLoader sceneLoader;
+
     public void Play()
     {
         Destroy(FindObjectOfType<AudioManager>().gameObject);
-        
+
         //Go straight to orchestra if finished tutorial, else start tutorial.
-        sceneLoader.LoadScene(PlayerPrefs.GetInt("TutorialComplete") > 0 
-            ? SceneLoader.Scene.Orchestra : SceneLoader.Scene.Tutorial);
+        sceneLoader.LoadScene(PlayerPrefs.GetInt("TutorialComplete") > 0
+            ? SceneLoader.Scene.Orchestra
+            : SceneLoader.Scene.Tutorial);
     }
 }
